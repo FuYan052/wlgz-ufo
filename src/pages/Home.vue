550c760f02c35ae6977710f43354bdbc2e7845b4
f <1622591668@qq.com>
2019-07-01 18:06:32 +0800

    feat:蔚蓝公装公众号项目骨架

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>前台首页</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="scss" scoped>
+  .home{
+
+  }
+</style>
+
---
+++ src/pages/backStage/BackstageHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ht_home">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackstageHome'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
